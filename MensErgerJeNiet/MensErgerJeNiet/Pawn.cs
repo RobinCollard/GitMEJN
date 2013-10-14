347797f298bf18982a2c9090f385e6e58af9a907
robincollard <name24@hotmail.nl>
2013-10-14 13:28:10 +0200

    Aanpassing levelbouwen

--- MensErgerJeNiet/MensErgerJeNiet/Pawn.cs
+++ MensErgerJeNiet/MensErgerJeNiet/Pawn.cs
@@ -1,1 +1,14 @@
-﻿
+﻿namespace MensErgerJeNiet
+{
+    public class Pawn
+    {
+        public Field MyField { get; set; }
+        public Color MyColor { get; set; }
+
+        public Pawn(Field myField, Color myColor)
+        {
+            this.MyField = myField;
+            this.MyColor = myColor;
+        }
+    }
+}
