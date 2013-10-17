5963c49085fd98b30166ca936cc10d2c741a5520
thimh <thim.heider@gmail.com>
2013-10-17 14:11:46 +0200

    else if voor number buttons

--- MensErgerJeNiet/MensErgerJeNiet/BoardView.xaml.cs
+++ MensErgerJeNiet/MensErgerJeNiet/BoardView.xaml.cs
@@ -345,6 +345,22 @@ namespace MensErgerJeNiet
                         current = current.Next;
                     }
                 }
+                else if(e.Key.Equals(Key.D1) || e.Key.Equals(Key.NumPad1))
+                {
+
+                }
+                else if(e.Key.Equals(Key.D2) || e.Key.Equals(Key.NumPad2))
+                {
+
+                }
+                else if(e.Key.Equals(Key.D3) || e.Key.Equals(Key.NumPad3))
+                {
+
+                }
+                else if(e.Key.Equals(Key.D4) || e.Key.Equals(Key.NumPad4))
+                {
+
+                }
             }
         }
 
